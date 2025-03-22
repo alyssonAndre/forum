a6cdadc77ef7885dcd0a34418981f6de8b3b9cda
Lucas Bastos <lbastos323@gmail.com>
2025-03-22 15:43:53 -0300

    :sparkles: FEAT: Melhora a lógica de carregamento de perguntas no teste vocacional e adiciona validação para perguntas respondidas

--- projeto/quizz/static/quizz/js/career_test.js
+++ projeto/quizz/static/quizz/js/career_test.js
@@ -1,5 +1,66 @@
+// let currentPage = 1;
+// let receivedQuestionIds = []; // Variável para armazenar os IDs das perguntas já recebidas
+
+// function loadQuestions() {
+//     // Cria a query string com os IDs das perguntas já recebidas
+//     let queryString = `page=${currentPage}`;
+//     if (receivedQuestionIds.length > 0) {
+//         queryString += '&received=' + receivedQuestionIds.join('&received=');
+//     }
+
+//     $.ajax({
+//         url: `/api/questions/?${queryString}`, // Inclui os IDs das perguntas já recebidas
+//         method: 'GET',
+//         success: function (data) {
+//             if (data.results.length > 0) {
+//                 data.results.forEach(function (question) {
+//                     // Adiciona o ID da pergunta à lista de perguntas recebidas
+//                     receivedQuestionIds.push(question.id);
+
+//                     let questionHtml = `
+//                         <div class="bg-white p-4 rounded-md shadow-md">
+//                             <h2 class="text-xl font-semibold mb-4">${question.text}</h2>
+//                             <ul class="space-y-2">
+//                     `;
+
+//                     question.alternatives.forEach(function (alternative) {
+//                         questionHtml += `
+//                             <li class="flex items-center space-x-4">
+//                                 <input type="radio" name="question_${question.id}" id="question_${question.id}_alternative_${alternative.id}" class="h-5 w-5">
+//                                 <label for="question_${question.id}_alternative_${alternative.id}" class="text-lg">${alternative.text}</label>
+//                             </li>
+//                         `;
+//                     });
+
+//                     questionHtml += `
+//                             </ul>
+//                         </div>
+//                     `;
+
+//                     $('#questions-container').append(questionHtml);
+//                 });
+
+//                 currentPage++;
+//             } else {
+//                 $('#load-more-questions').prop('disabled', true).text('Nenhuma pergunta disponível');
+//             }
+//         },
+//         error: function () {
+//             alert('Erro ao carregar perguntas!');
+//         }
+//     });
+// }
+
+// $(document).ready(function () {
+//     loadQuestions();
+
+//     $('#load-more-questions').click(function () {
+//         loadQuestions();
+//     });
+// });
 let currentPage = 1;
 let receivedQuestionIds = []; // Variável para armazenar os IDs das perguntas já recebidas
+let questionCount = 0; // Contador de perguntas carregadas
 
 function loadQuestions() {
     // Cria a query string com os IDs das perguntas já recebidas
@@ -13,48 +74,86 @@ function loadQuestions() {
         method: 'GET',
         success: function (data) {
             if (data.results.length > 0) {
-                data.results.forEach(function (question) {
-                    // Adiciona o ID da pergunta à lista de perguntas recebidas
-                    receivedQuestionIds.push(question.id);
+                const question = data.results[0]; // Carrega apenas a primeira pergunta do lote retornado
+                // Adiciona o ID da pergunta à lista de perguntas recebidas
+                receivedQuestionIds.push(question.id);
 
-                    let questionHtml = `
-                        <div class="bg-white p-4 rounded-md shadow-md">
-                            <h2 class="text-xl font-semibold mb-4">${question.text}</h2>
-                            <ul class="space-y-2">
-                    `;
+                // Incrementa o contador de perguntas
+                questionCount++;
 
-                    question.alternatives.forEach(function (alternative) {
-                        questionHtml += `
-                            <li class="flex items-center space-x-4">
-                                <input type="radio" name="question_${question.id}" id="question_${question.id}_alternative_${alternative.id}" class="h-5 w-5">
-                                <label for="question_${question.id}_alternative_${alternative.id}" class="text-lg">${alternative.text}</label>
-                            </li>
-                        `;
-                    });
+                let questionHtml = `
+                    <div class="bg-white p-4 rounded-md shadow-md">
+                        <h2 class="text-xl font-semibold mb-4">${questionCount}. ${question.text}</h2>
+                        <ul class="space-y-2">
+                        <span id="message-container" class="text-red-500 hidden warning-message text-sm"></span>
+                `;
 
+                question.alternatives.forEach(function (alternative) {
                     questionHtml += `
-                            </ul>
-                        </div>
+                        <li class="flex items-center space-x-4">
+                            <input type="radio" name="question_${question.id}" id="question_${question.id}_alternative_${alternative.id}" class="h-5 w-5">
+                            <label for="question_${question.id}_alternative_${alternative.id}" class="text-lg">${alternative.text}</label>
+                        </li>
                     `;
-
-                    $('#questions-container').append(questionHtml);
                 });
 
+                questionHtml += `
+                        </ul>
+                    </div>
+                `;
+
+                // Substitui o conteúdo de #questions-container pela nova pergunta
+                $('#questions-container').html(questionHtml);
+
                 currentPage++;
+
+                // Atualiza o botão para "Finalizar teste" após a última pergunta
+                if (questionCount >= 10) {
+                    $('#load-more-questions').text('Finalizar teste');
+                }
             } else {
                 $('#load-more-questions').prop('disabled', true).text('Nenhuma pergunta disponível');
             }
         },
         error: function () {
-            alert('Erro ao carregar perguntas!');
+            showMessage('Erro ao carregar perguntas!');
         }
     });
+}
+
+function checkAnsweredQuestions() {
+    let allAnswered = true;
+    $('div[id^="questions-container"] input[type="radio"]').each(function () {
+        let name = $(this).attr('name');
+        if (!$(`input[name="${name}"]:checked`).length) {
+            allAnswered = false;
+            return false; // Para de iterar se encontrar uma pergunta não respondida
+        }
+    });
+    return allAnswered;
+}
+
+function showMessage(message) {
+    $('#message-container').text(message).removeClass('hidden').fadeIn();
+    setTimeout(function () {
+        $('#message-container').fadeOut();
+    }, 3000);
 }
 
 $(document).ready(function () {
     loadQuestions();
 
     $('#load-more-questions').click(function () {
-        loadQuestions();
+        if ($(this).text() === 'Finalizar teste') {
+            if (checkAnsweredQuestions()) {
+                alert("Teste finalizado! Obrigado por participar!");
+            } else {
+                showMessage('Por favor, responda a pergunta antes de finalizar o teste!');
+            }
+        } else if (checkAnsweredQuestions()) {
+            loadQuestions();
+        } else {
+            showMessage('Por favor, responda a pergunta antes de prosseguir!');
+        }
     });
 });
