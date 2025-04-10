ac12fe4a23b1a0ea3aaee6275dd14955d5c5c148
alysson <maguslim.silva364@gmail.com>
2025-04-10 19:49:51 -0300

    :wrench: fix: changed codes in admin.py

--- projeto/quizz/admin.py
+++ projeto/quizz/admin.py
@@ -3,29 +3,22 @@ from .models import Quiz, Question, Alternative, UserAnswer
 from .models.Base import Course
 
 
-# Registro do modelo Quiz
-@admin.register(Quiz)
+
 class QuizAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
     search_fields = ('title',)
     list_filter = ('title',)
 
-# Registro do modelo Question
-@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('text', 'quiz')
     search_fields = ('text',)
     list_filter = ('quiz',)
 
-# Registro do modelo Alternative
-@admin.register(Alternative)
 class AlternativeAdmin(admin.ModelAdmin):
     list_display = ('text', 'question', 'score', 'recommended_course')
     search_fields = ('text',)
     list_filter = ('question', 'recommended_course')
 
-# Registro do modelo UserAnswer
-@admin.register(UserAnswer)
 class UserAnswerAdmin(admin.ModelAdmin):
     list_display = ('user', 'quiz', 'question', 'alternative', 'data')
     search_fields = ('user__username', 'quiz__title', 'question__text')
@@ -38,4 +31,10 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ('type', 'is_deleted')
     ordering = ('name',)
 
+
+# Registro de todos os modelos
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Alternative, AlternativeAdmin)
+admin.site.register(UserAnswer, UserAnswerAdmin)
 admin.site.register(Course, CourseAdmin)
