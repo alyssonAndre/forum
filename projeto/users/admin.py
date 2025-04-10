12748fa2657a029178547b495fb1974f26bf2194
alysson <maguslim.silva364@gmail.com>
2025-04-10 19:53:04 -0300

    :sparkles: feat: added new models in admin.py

--- projeto/users/admin.py
+++ projeto/users/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'courses')
+
+
 
 admin.site.unregister(User)
-
 admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile,UserProfileAdmin)
