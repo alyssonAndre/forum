42f0d957cf9f7b04484a4949087495a8d249e93c
Lucas Bastos <lbastos323@gmail.com>
2025-04-19 15:26:49 -0300

    :sparkles: feat: adiciona redirecionamento para a página de quiz na raiz do site

--- projeto/setup/urls.py
+++ projeto/setup/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('api/', include('quizz.api_urls')),
     path('users/', include('users.urls'), name='users'),
     path('accounts/', include('allauth.urls')),
+    path('', lambda request: redirect('quiz/', permanent=False)),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
