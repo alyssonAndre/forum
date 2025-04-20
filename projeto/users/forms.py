05d60d3dedabcbb8f08b5f1a75b4ca5c3b46d6f0
alysson <maguslim.silva364@gmail.com>
2025-04-20 16:47:03 -0300

    :sparkles: feat: added functions for accept login with email and username

--- projeto/users/forms.py
+++ projeto/users/forms.py
@@ -1,15 +1,36 @@
 from django import forms
+from django.contrib.auth import get_backends,authenticate
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-class CustomLoginForm():
-    email = forms.EmailField(required=True)
-    password = forms.CharField(max_length=30, required=True)
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Username or Email', required=True)
 
-    class Meta:
-        model = User
-        fields = ('email', 'password')
+    def clean(self):
+        username_or_email = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username_or_email and password:
+            self.user_cache = authenticate(self.request, username=username_or_email, password=password)
+
+            if self.user_cache is None:
+                try:
+                    user = User.objects.get(email=username_or_email)
+                    self.user_cache = authenticate(self.request, username=user.username, password=password)
+                except User.DoesNotExist:
+                    pass
+
+            if self.user_cache is None:
+                raise forms.ValidationError("Invalid username or email, or incorrect password.")
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError("This account is inactive.")
+
+            backend = self.user_cache.backend
+            self.request.session['_auth_user_backend'] = backend
+
+        return self.cleaned_data
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
--- projeto/users/views.py
+++ projeto/users/views.py
@@ -1,12 +1,22 @@
+from django.shortcuts import redirect
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView
 from .forms import CustomUserCreationForm, CustomLoginForm
 
+
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'
-    form = CustomLoginForm
+    authentication_form = CustomLoginForm
+    success_url = reverse_lazy('quizz:index')
+
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.info(request, 'You are already logged in.')
+            return redirect(self.get_success_url())
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self,form):
         messages.success(self.request, 'Login successful')
