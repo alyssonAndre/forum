1206a64a9f52fc81a7e18ca1f89d8d3169a7ee15
Lucas Bastos <lbastos323@gmail.com>
2025-05-17 18:06:19 -0300

    :sparkles: feat: adiciona redirecionamento para login em post_list, post_detail e post_create

--- projeto/forum/views.py
+++ projeto/forum/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 
 def post_list(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'forum/post_list.html', {'posts': posts})
 
 def post_detail(request, post_id):
+    if not request.user.is_authenticated:
+        return redirect('login')
     post = get_object_or_404(Post, id=post_id)
     comments = Comment.objects.filter(post=post).order_by('created_at')
     return render(request, 'forum/post_detail.html', {'post': post, 'comments': comments})
+
+def post_create(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'forum/post_create.html')
