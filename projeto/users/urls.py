9e157aac8e7a52f2159b158bf3e1ca2b560a75a0
Lucas Bastos <lbastos323@gmail.com>
2025-05-17 18:07:17 -0300

    :sparkles: feat: adiciona rota para visualização de perfil do usuário

--- projeto/users/urls.py
+++ projeto/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomLoginView, RegisterView, CustomLogoutView
+from .views import CustomLoginView, RegisterView, CustomLogoutView, CustomProfileView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('profile/', CustomProfileView.as_view(), name='profile'),
 ]
