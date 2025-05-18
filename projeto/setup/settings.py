0a93501cd5e3a542c6bc3a21c67f944a456f32c6
alysson <maguslim.silva364@gmail.com>
2025-05-17 23:50:17 -0300

    :sparkles: feat: add new variables for images

--- projeto/setup/settings.py
+++ projeto/setup/settings.py
@@ -146,6 +146,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "forum" / "static",  # Inclui a pasta 'static' dentro de 'forum'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
