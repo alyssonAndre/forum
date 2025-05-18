a759afdc17a723778a333a2c6ad794a679dbce94
alysson <maguslim.silva364@gmail.com>
2025-05-17 23:55:36 -0300

    :wrench: feat: refactoring code

--- projeto/users/views.py
+++ projeto/users/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect,render
 from django.urls import reverse_lazy
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth import logout
-from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
-from .forms import CustomUserCreationForm, CustomLoginForm
+from .forms import CustomUserCreationForm, CustomLoginForm, UserProfileForm, ProfilePhotoForm
 
 
 class CustomLoginView(LoginView):
@@ -51,7 +53,32 @@ class CustomLogoutView(View):
         messages.success(request, 'You have been logged out.')
         return redirect('index')
 
-class CustomProfileView(LoginView):
+
+
+class CustomProfileView(LoginRequiredMixin, View):
     template_name = 'users/profile.html'
-    authentication_form = CustomLoginForm
-    success_url = reverse_lazy('quizz:index')
+    login_url = reverse_lazy('login')
+
+    def get(self, request):
+        user_form = UserProfileForm(instance=request.user)
+        profile_form = ProfilePhotoForm(instance=request.user.profile)
+        return render(request, self.template_name, {
+            'form': user_form,
+            'profile_form': profile_form
+        })
+
+    def post(self, request):
+        user_form = UserProfileForm(request.POST, instance=request.user)
+        profile_form = ProfilePhotoForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Perfil atualizado com sucesso!")
+            return redirect('profile')
+        else:
+            messages.error(request, "Erro ao atualizar o perfil.")
+            return render(request, self.template_name, {
+                'form': user_form,
+                'profile_form': profile_form
+            })
