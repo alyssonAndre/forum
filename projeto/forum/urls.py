500cb431e5f32d0b372855cfeee675ce39099b92
alysson <maguslim.silva364@gmail.com>
2025-05-17 23:46:58 -0300

    :wrench: feat: change de FBV to CBV

--- projeto/forum/urls.py
+++ projeto/forum/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PostCreateView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('create-post/', views.post_create, name='post_create'),
+    path('create-post/', PostCreateView.as_view(), name='post_create'),
 ]
