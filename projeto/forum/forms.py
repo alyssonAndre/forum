db75d8d6e04b930b588ecea121cb7a596ce040df
alysson <maguslim.silva364@gmail.com>
2025-05-17 23:56:13 -0300

    :sparkles: create forms for post

---
+++ projeto/forum/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Post
+
+class PostCreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'required': True,
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none min-h-[150px]',
+                'required': True,
+                'rows': 6,
+            }),
+        }
